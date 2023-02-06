c0ea2b815c4a70e1e3737479538e5b52cb01a531
Noël Maccary <n.maccary@lexfo.fr>
2023-02-06 17:09:49 +0100

    docs: Add comments.

--- scalpel/Main.java
+++ scalpel/Main.java
@@ -7,16 +7,21 @@ import java.lang.System;
 public class Main {
 
     public static void main(String[] args) {
+        // Python script path.
         var script = "scripts/myscript.py";
 
         try (Interpreter interp = new SharedInterpreter()) {
+            // Running Python instructions on the fly.
             System.out.println("------- Running \"on the fly\" Python -------");
+
             interp.exec("from java.lang import System");
             interp.exec("s = 'Hello World'");
             interp.exec("System.out.println(s)");
             interp.exec("print(f'F strings are working: {s + \"_World\"}')");
             interp.exec("print(s[1:-1])");
+
             System.out.println(String.format("------- Running %s ---------------", script));
+            // Running a python script file.
             interp.runScript(script);
         }
     }
