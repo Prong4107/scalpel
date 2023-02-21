ed66d2213d87b86adfaa24370851bedef1aea3e7
Noël Maccary <n.maccary@lexfo.fr>
2023-02-21 11:51:22 +0100

    feature(callbacks): Allow request modification.

--- scripts/requestTest.py
+++ scripts/requestTest.py
@@ -1,3 +1,4 @@
 # Example request callback script
 def request(req, text, tab_name, logger):
     logger.logToOutput(f"tab_name: {tab_name},\n\nreq: {req},\n\ntext: {text}\n\n-------------")
+    return req.withAddedHeader("X-CustomPythonScript", "Hello")
