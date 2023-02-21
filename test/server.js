238a5ff0b61597cfc78735c17008af9ce7502f65
Noël Maccary <n.maccary@lexfo.fr>
2023-02-21 11:20:36 +0100

    test: Add express test HTTP server

--- test/server.js
+++ test/server.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')()
+app.use(require('body-parser').raw({ type: String }))
 
 app.all('/', (req, res) => {
-    req.
-  res.send({url: req.url, headers: req.headers, body: req.body})
+    res.send({
+        url: req.url,
+        headers: (
+            () => {
+                let key; return Object.assign({}, ...req.rawHeaders.map(
+                    (val, i) => i & 1 ? { [key]: val } : (key = val) && null).filter(e => e)
+                )
+            }
+        )(),
+        body: req.body.entries && req.body.toString()
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000)
+
