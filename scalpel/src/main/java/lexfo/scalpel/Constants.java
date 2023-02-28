5ead0a0044604752316ba697046c454855930cfa
Noël Maccary <n.maccary@lexfo.fr>
2023-02-28 11:12:51 +0100

    refactor(callbacks): Improve callbacks handling.

--- scalpel/src/main/java/lexfo/scalpel/Constants.java
+++ scalpel/src/main/java/lexfo/scalpel/Constants.java
@@ -1,6 +1,8 @@
 package lexfo.scalpel;
 
 public class Constants {
-    public static final String REQ_EDIT_IN_CB_PREFIX = "req_edit_";
-    public static final String RES_EDIT_IN_CB_PREFIX = "res_edit_";
+    public static final String REQ_EDIT_PREFIX = "req_edit_";
+    public static final String RES_EDIT_PREFIX = "res_edit_";
+    public static final String IN_PREFIX = "in_";
+    public static final String OUT_PREFIX = "out_";
 }
