b8c5ce32acbe14143199135236171117714a2d6f
Noël Maccary <n.maccary@lexfo.fr>
2023-03-03 14:18:13 +0100

    refactor(exampleScript): Add types to example

--- scripts/editorTest.py
+++ scripts/editorTest.py
@@ -1,45 +1,48 @@
-from burp.api.montoya.http.message.requests import HttpRequest
-from burp.api.montoya.core import ByteArray
+from burp.api.montoya.http.message.requests import HttpRequest as BurpHttpRequest
+from burp.api.montoya.http.message.responses import HttpResponse as BurpHttpResponse
+from burp.api.montoya.core import ByteArray as BurpByteArray
 
+from burpTypes import HttpRequest, ByteArray, Logging, HttpResponse
+from sys import _getframe
 
 # Test script that adds debug headers
 
-def request(req, logger=None):
+def funName(  ):
+    return _getframe(1).f_code.co_name
+
+
+def request(req: HttpRequest, logger: Logging=...) -> HttpRequest | None:
     try:
-        logger.logToOutput("Python: request() called")
+        logger.logToOutput(f"Python: {funName()}() called")
         return req.withUpdatedHeader("X-Python-Intercept-Request", "Hello")
-    except:
-        logger.logToError("Python: error")
+    except Exception as e:
+        logger.logToError(f"Python: {funName()}() error:\n\t{e}")
     return req
 
 
-def response(res, logger=None):
-    logger.logToOutput("Python: response() called")
+def response(res: HttpResponse, logger: Logging=...) -> HttpResponse | None:
+    logger.logToOutput(f"Python: {funName()}() called")
     return res.withUpdatedHeader("X-Python-Intercept-Response", "true")
 
 # OK
-def req_edit_in_Scalpel(req, logger=None):
+def req_edit_in_Scalpel(req: HttpRequest, logger: Logging=...) -> HttpRequest | None:
     try:
-        logger.logToOutput("Python: req_edit_in_...() called")
-        logger.logToOutput(f"{req.getClass()}")
-        return req.withUpdatedHeader("X-Python-In-Request-Editor", "true").toByteArray().getBytes()
+        logger.logToOutput("Python: req_edit_in_...() {funName()}() called")
+        return BurpHttpRequest.httpRequest(req.toByteArray()).withAddedHeader("X-Python-In-Request-Editor", "true").toByteArray().getBytes()
     except Exception as e:
-        logger.logToError(f"py req_in err: \n\t{e}")
+        logger.logToError(f"Python: {funName()}() error:\n\t{e}")
 
 # OK
-def req_edit_out_Scalpel(req, text: bytes, logger=None):
-    logger.logToOutput("Python: req_edit_out_...() called")
+def req_edit_out_Scalpel(req: HttpRequest, text: bytes, logger: Logging=...) -> HttpRequest | None:
+    logger.logToOutput(f"Python: {funName()}() called")
     logger.logToOutput(f"Python: {text}")
-    return HttpRequest.httpRequest(ByteArray.byteArray(text)).withUpdatedHeader("X-Python-Out-Request-Editor", "true").toByteArray().getBytes()
+    return BurpHttpRequest.httpRequest(ByteArray.byteArray(text)).withUpdatedHeader("X-Python-Out-Request-Editor", "true").toByteArray().getBytes()  # type: ignore
 
-def res_edit_in_Scalpel(res, logger=None):
+# Broken
+def res_edit_in_Scalpel(res: HttpResponse, logger: Logging=...) -> HttpResponse | None:
     try:
-        logger.logToOutput("Python: res_edit_in_...() called")
-        return res.withUpdatedHeader("X-Python-In-Response-Editor", "true").toByteArray().getBytes()
+        logger.logToOutput(f"Python: {funName()}() called")
+        return BurpHttpResponse.httpResponse(res.toByteArray()).withAddedHeader("X-Python-In-Response-Editor", "true").toByteArray().getBytes()
     except Exception as e:
-        logger.logToError(f"py res_in errr: \n\t{e}")
+        logger.logToError(f"Python: {funName()}() error:\n\t{e}")
 
-# KO
-# def req_edit_Scalpel(req, logger):s
-#     logger.logToOutput("called")
-#     return 0xDEADBEEF
