7e29b0ac7061bfc3de825614158dcabddf11152c
Noël Maccary <n.maccary@lexfo.fr>
2023-03-03 14:08:29 +0100

    refactor: Rm old file

--- scripts/requestTest.py
+++
@@ -1,8 +0,0 @@
-# Example request callback script
-def request(req, text, tab_name, logger):
-    logger.logToOutput(f"tab_name: {tab_name},\n\nreq: {req},\n\ntext: {text}\n\n-------------")
-    return req.withAddedHeader("X-Python-Request", "Hello")
-
-def response(res, logger):
-    logger.logToOutput(f"res: {res}")
-    return res.withAddedHeader("X-Python-Response", "Hello")
