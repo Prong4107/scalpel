da279d62090061ed48636e5a577046f24b742101
Noël Maccary <n.maccary@lexfo.fr>
2023-03-14 11:57:21 +0100

    test(base64): Add base64 test case

--- test/server.js
+++ test/server.js
@@ -1,21 +1,32 @@
 const app = require('express')()
 app.use(require('body-parser').raw({ type: String }))
 
+jsonifyRequest =  (req) => ({
+    url: req.url,
+    headers: (
+        () => {
+            let key; return Object.assign({}, ...req.rawHeaders.map(
+                (val, i) => i & 1 ? { [key]: val } : (key = val) && null).filter(e => e)
+            )
+        }
+    )(),
+    body: req.body.entries && req.body.toString()
+})
+
+app.all('/base64', (req, res) => {
+    console.log("Received /base64")
+    // Remove date header to ensure identical requests returns the exact same response
+    res.setHeader("Date", "[REDACTED]")
+    const decoded = new Buffer(req.body.toString('utf-8'), "base64").toString('utf-8');
+
+    res.send(Buffer.from(`Received in base64:\n-----\n${decoded}\n-----`).toString('base64'));
+})
+
 app.all('/', (req, res) => {
     console.log("Received")
     // Remove date header to ensure identical requests returns the exact same response
     res.setHeader("Date", "[REDACTED]")
-    res.send({
-        url: req.url,
-        headers: (
-            () => {
-                let key; return Object.assign({}, ...req.rawHeaders.map(
-                    (val, i) => i & 1 ? { [key]: val } : (key = val) && null).filter(e => e)
-                )
-            }
-        )(),
-        body: req.body.entries && req.body.toString()
-    })
+    res.send(jsonifyRequest(req))
 })
 
 app.listen(3000)
