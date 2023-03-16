b417641f349c60dae3abca84ed037623f3795e19
Noël Maccary <n.maccary@lexfo.fr>
2023-03-16 16:21:41 +0100

    refactor(logger): Add logs to stdout and stderr

--- scalpel/src/main/java/lexfo/scalpel/TraceLogger.java
+++ scalpel/src/main/java/lexfo/scalpel/TraceLogger.java
@@ -5,25 +5,34 @@ import java.util.Arrays;
 
 public class TraceLogger {
 
+  static void log(Logging logger, String msg) {
+    System.out.println(msg);
+    logger.logToOutput(msg);
+  }
+
+  static void logError(Logging logger, String msg) {
+    System.err.println(msg);
+    logger.logToError(msg);
+  }
   static void logStackTrace(Logging logger, Throwable throwed) {
-    logger.logToError("ERROR:");
-    logger.logToError(throwed.toString());
+    logError(logger, "ERROR:");
+    logError(logger, throwed.toString());
     Arrays
       .stream(throwed.getStackTrace())
-      .forEach(el -> logger.logToError(el.toString()));
+      .forEach(el -> logError(logger, el.toString()));
   }
 
   static void logStackTrace(Logging logger) {
     Arrays
       .stream(Thread.currentThread().getStackTrace())
-      .forEach(el -> logger.logToError(el.toString()));
+      .forEach(el -> logError(logger, el.toString()));
   }
 
   static void logStackTrace(Logging logger, Boolean error) {
     Arrays
       .stream(Thread.currentThread().getStackTrace())
       .forEach(el -> {
-        if (error) logger.logToError(el.toString()); else logger.logToOutput(
+        if (error) logError(logger, el.toString()); else logger.logToOutput(
           el.toString()
         );
       });
