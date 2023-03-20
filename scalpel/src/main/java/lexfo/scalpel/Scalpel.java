f3dc0236534e56eee6fd9981338453959288a423
Noël Maccary <n.maccary@lexfo.fr>
2023-03-20 18:07:05 +0100

    style: Reformatted Java sources

--- scalpel/src/main/java/lexfo/scalpel/Scalpel.java
+++ scalpel/src/main/java/lexfo/scalpel/Scalpel.java
@@ -12,95 +12,86 @@ import jep.MainInterpreter;
 */
 public class Scalpel implements BurpExtension {
 
-  /**
+	/**
     The logger object used to log messages to Burp Suite's output tab.
   */
-  private Logging logger;
+	private Logging logger;
 
-  /**
+	/**
     The ScalpelUnpacker object used to extract the extension's resources to a temporary directory.
   */
-  private ScalpelUnpacker unpacker;
+	private ScalpelUnpacker unpacker;
 
-  /**
+	/**
     The ScalpelExecutor object used to execute Python scripts.
   */
-  private ScalpelExecutor executor;
+	private ScalpelExecutor executor;
 
-  /**
+	/**
     The MontoyaApi object used to interact with Burp Suite.
   */
-  private MontoyaApi API;
+	private MontoyaApi API;
 
-  /**
+	/**
     Initializes the extension.
     @param API The MontoyaApi object to use.
   */
-  @Override
-  public void initialize(MontoyaApi API) {
-    // Init API member.
-    this.API = API;
+	@Override
+	public void initialize(MontoyaApi API) {
+		// Init API member.
+		this.API = API;
 
-    // Set extension name.
-    API.extension().setName("Lexfo Scalpel extension");
+		// Set extension name.
+		API.extension().setName("Lexfo Scalpel extension");
 
-    // Init logger member.
-    logger = API.logging();
+		// Init logger member.
+		logger = API.logging();
 
-    // Show that the extension is loading.
-    logger.logToOutput("Initializing...");
+		// Show that the extension is loading.
+		logger.logToOutput("Initializing...");
 
-    // Change this to stop Python from being initialized (for JEP debugging purposes)
-    Boolean initPython = true;
+		// Change this to stop Python from being initialized (for JEP debugging purposes)
+		Boolean initPython = true;
 
-    // Init JEP stuff.
-    if (initPython) {
-      // Instantiate unpacker
-      unpacker = new ScalpelUnpacker(logger);
+		// Init JEP stuff.
+		if (initPython) {
+			// Instantiate unpacker
+			unpacker = new ScalpelUnpacker(logger);
 
-      // Extract the ressources to a new unique temporary directory.
-      unpacker.initializeResourcesDirectory();
+			// Extract the ressources to a new unique temporary directory.
+			unpacker.initializeResourcesDirectory();
 
-      // Set Jep path.
-      MainInterpreter.setJepLibraryPath(
-        unpacker.getResourcesPath() + "/libjep.so"
-      );
+			// Set Jep path.
+			MainInterpreter.setJepLibraryPath(unpacker.getResourcesPath() + "/libjep.so");
 
-      // Instantiate the executor (handles Python execution)
-      executor =
-        new ScalpelExecutor(
-          API,
-          logger,
-          // TODO: CHANGE ME!
-          "/home/nol/Desktop/piperpp/scalpel/scripts/editorTest.py"
-        );
+			// Instantiate the executor (handles Python execution)
+			executor =
+				new ScalpelExecutor(
+					API,
+					logger,
+					// TODO: CHANGE ME!
+					"/home/nol/Desktop/piperpp/scalpel/scripts/editorTest.py"
+				);
 
-      // Add the scripting editor tab.
-      API
-        .userInterface()
-        .registerSuiteTab(
-          "Scalpel Interpreter",
-          UIBuilder.constructScalpelInterpreterTab(executor, logger)
-        );
+			// Add the scripting editor tab.
+			API
+				.userInterface()
+				.registerSuiteTab("Scalpel Interpreter", UIBuilder.constructScalpelInterpreterTab(executor, logger));
 
-      // Add request editor tab
-      var provider = new ScalpelEditorProvider(API, executor);
+			// Add request editor tab
+			var provider = new ScalpelEditorProvider(API, executor);
 
-      // Add the request editor to Burp.
-      API.userInterface().registerHttpRequestEditorProvider(provider);
+			// Add the request editor to Burp.
+			API.userInterface().registerHttpRequestEditorProvider(provider);
 
-      // Add the response editor to Burp.
-      API.userInterface().registerHttpResponseEditorProvider(provider);
+			// Add the response editor to Burp.
+			API.userInterface().registerHttpResponseEditorProvider(provider);
 
-      // Add an HTTP intercepter to Burp.
-      API
-        .http()
-        .registerHttpHandler(
-          new ScalpelHttpRequestHandler(API, provider, executor)
-        );
-    }
+			// Add an HTTP intercepter to Burp.
+			API.http().registerHttpHandler(new ScalpelHttpRequestHandler(API, provider, executor));
+		}
 
-    // Log that the extension has finished loading.
-    logger.logToOutput("Initialized successfully.");
-  }
+		// Log that the extension has finished loading.
+		logger.logToOutput("Initialized successfully.");
+	}
 }
--- scalpel/src/main/java/lexfo/scalpel/UIBuilder.java
+++ scalpel/src/main/java/lexfo/scalpel/UIBuilder.java
@@ -1,74 +1,61 @@
 package lexfo.scalpel;
 
+import burp.api.montoya.logging.Logging;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import burp.api.montoya.logging.Logging;
 
 /**
-  Provides methods for constructing the Burp Suite UI.
+	Provides methods for constructing the Burp Suite UI.
 */
 public class UIBuilder {
 
+	/**
+		Constructs the debug Python testing Burp tab.
+		@param executor The ScalpelExecutor object to use.
+		@param logger The Logging object to use.
+		@return The constructed tab.
+	*/
+	public static final Component constructScalpelInterpreterTab(ScalpelExecutor executor, Logging logger) {
+		// Split pane
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		JSplitPane scriptingPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JTextArea outputArea = new JTextArea();
+		JEditorPane editorPane = new JEditorPane();
+		JButton button = new JButton("Run script.");
 
-  // Unused configuration tab constructor
-  /**
-    Constructs the configuration Burp tab.
-    @param executor The ScalpelExecutor object to use.
-    @return The constructed tab.
-  */
-  public final static Component constructConfigTab(ScalpelExecutor executor) {
-    return null;
-  }
+		button.addActionListener((ActionEvent e) -> {
+			logger.logToOutput("Clicked button");
+			String scriptContent = editorPane.getText();
+			try {
+				String[] scriptOutput = executor.evalAndCaptureOutput(scriptContent);
 
-  /**
-    Constructs the debug Python testing Burp tab.
-    @param executor The ScalpelExecutor object to use.
-    @param logger The Logging object to use.
-    @return The constructed tab.
-  */
-  public final static Component constructScalpelInterpreterTab(ScalpelExecutor executor, Logging logger) {
-    // Split pane
-    JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-    JSplitPane scriptingPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-    JTextArea outputArea = new JTextArea();
-    JEditorPane editorPane = new JEditorPane();
-    JButton button = new JButton("Run script.");
+				var txt = String.format(
+					"stdout:\n------------------\n%s\n------------------\n\nstderr:\n------------------\n%s",
+					scriptOutput[0],
+					scriptOutput[1]
+				);
 
-    button.addActionListener((ActionEvent e) -> {
-      logger.logToOutput("Clicked button");
-      String scriptContent = editorPane.getText();
-      try {
-        String[] scriptOutput = executor.evalAndCaptureOutput(scriptContent);
+				outputArea.setText(txt);
+			} catch (Exception exception) {
+				outputArea.setText(exception.toString());
+			}
+			logger.logToOutput("Handled action.");
+		});
 
-        var txt = String.format(
-          "stdout:\n------------------\n%s\n------------------\n\nstderr:\n------------------\n%s",
-          scriptOutput[0],
-          scriptOutput[1]
-        );
-
-        outputArea.setText(txt);
-      } catch (Exception exception) {
-        outputArea.setText(exception.toString());
-      }
-      logger.logToOutput("Handled action.");
-    });
-
-    editorPane.setText(
-      """
+		editorPane.setText("""
 print('This goes in stdout')
 print('This goes in stderr', file=sys.stderr)
-"""
-    );
-    outputArea.setEditable(false);
-    scriptingPane.setLeftComponent(editorPane);
-    scriptingPane.setRightComponent(outputArea);
-    scriptingPane.setResizeWeight(0.5);
+""");
+		outputArea.setEditable(false);
+		scriptingPane.setLeftComponent(editorPane);
+		scriptingPane.setRightComponent(outputArea);
+		scriptingPane.setResizeWeight(0.5);
 
-    splitPane.setResizeWeight(1);
-    splitPane.setLeftComponent(scriptingPane);
-    splitPane.setRightComponent(button);
+		splitPane.setResizeWeight(1);
+		splitPane.setLeftComponent(scriptingPane);
+		splitPane.setRightComponent(button);
 
-    return splitPane;
-  }
+		return splitPane;
+	}
 }
