e922e95cb90c333b48191fef2cd63178a8f50aa8
Noël Maccary <n.maccary@lexfo.fr>
2023-03-23 16:41:48 +0100

    refactor: Format, change logger, update UI, catch

--- scalpel/src/main/java/lexfo/scalpel/Scalpel.java
+++ scalpel/src/main/java/lexfo/scalpel/Scalpel.java
@@ -5,7 +5,7 @@ import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.logging.Logging;
 import jep.MainInterpreter;
 
-//Burp will auto-detect and load any class that extends BurpExtension.
+// Burp will auto-detect and load any class that extends BurpExtension.
 /**
   The main class of the extension.
   This class is instantiated by Burp Suite and is used to initialize the extension.
@@ -13,29 +13,29 @@ import jep.MainInterpreter;
 public class Scalpel implements BurpExtension {
 
 	/**
-    The logger object used to log messages to Burp Suite's output tab.
-  */
+	 * The logger object used to log messages to Burp Suite's output tab.
+	 */
 	private Logging logger;
 
 	/**
-    The ScalpelUnpacker object used to extract the extension's resources to a temporary directory.
-  */
+	 * The ScalpelUnpacker object used to extract the extension's resources to a temporary directory.
+	 */
 	private ScalpelUnpacker unpacker;
 
 	/**
-    The ScalpelExecutor object used to execute Python scripts.
-  */
+	 * The ScalpelExecutor object used to execute Python scripts.
+	 */
 	private ScalpelExecutor executor;
 
 	/**
-    The MontoyaApi object used to interact with Burp Suite.
-  */
+	 * The MontoyaApi object used to interact with Burp Suite.
+	 */
 	private MontoyaApi API;
 
 	/**
-    Initializes the extension.
+     * Initializes the extension.
     @param API The MontoyaApi object to use.
-  */
+	*/
 	@Override
 	public void initialize(MontoyaApi API) {
 		// Init API member.
@@ -47,58 +47,71 @@ public class Scalpel implements BurpExtension {
 		// Init logger member.
 		logger = API.logging();
 
-		// Show that the extension is loading.
-		logger.logToOutput("Initializing...");
+		try {
+			// Show that the extension is loading.
+			TraceLogger.log(logger, "Initializing...");
 
-		// Change this to stop Python from being initialized (for JEP debugging purposes)
-		Boolean initPython = true;
+			// Change this to stop Python from being initialized (for JEP debugging purposes)
+			final Boolean initPython = true;
 
-		// Init JEP stuff.
-		if (initPython) {
-			// Instantiate unpacker
-			unpacker = new ScalpelUnpacker(logger);
+			// Init JEP stuff.
+			if (initPython) {
+				// Instantiate unpacker
+				unpacker = new ScalpelUnpacker(logger);
 
-			// Extract the ressources to a new unique temporary directory.
-			unpacker.initializeResourcesDirectory();
+				// Extract the ressources to a new unique temporary directory.
+				unpacker.initializeResourcesDirectory();
 
-			// Set Jep path.
-			MainInterpreter.setJepLibraryPath(unpacker.getResourcesPath() + "/libjep.so");
-
-			// Instantiate the executor (handles Python execution)
-			executor =
-				new ScalpelExecutor(
-					API,
-					logger,
-					// TODO: CHANGE ME!
-					"/home/nol/Desktop/piperpp/scalpel/scripts/editorTest.py"
+				// Set Jep path.
+				MainInterpreter.setJepLibraryPath(
+					unpacker.getResourcesPath() + "/libjep.so"
 				);
 
-			// Add the scripting editor tab.
-			API
-				.userInterface()
-				.registerSuiteTab("Scalpel Interpreter", UIBuilder.constructScalpelInterpreterTab(executor, logger));
+				// Instantiate the executor (handles Python execution)
+				executor = new ScalpelExecutor(API, logger);
 
-			API
-				.userInterface()
-				.registerSuiteTab(
-					"Scalpel Config",
-					UIBuilder.constructConfigTab(executor, "/home/nol/Desktop/piperpp/scalpel/scripts/editorTest.py")
-				);
+				// Add the scripting editor tab.
+				API
+					.userInterface()
+					.registerSuiteTab(
+						"Scalpel Interpreter",
+						UIBuilder.constructScalpelInterpreterTab(
+							executor,
+							logger
+						)
+					);
 
-			// Add request editor tab
-			var provider = new ScalpelEditorProvider(API, executor);
+				API
+					.userInterface()
+					.registerSuiteTab(
+						"Scalpel Config",
+						UIBuilder.constructConfigTab(API, executor)
+					);
 
-			// Add the request editor to Burp.
-			API.userInterface().registerHttpRequestEditorProvider(provider);
+				// Add request editor tab
+				final var provider = new ScalpelEditorProvider(API, executor);
 
-			// Add the response editor to Burp.
-			API.userInterface().registerHttpResponseEditorProvider(provider);
+				// Add the request editor to Burp.
+				API.userInterface().registerHttpRequestEditorProvider(provider);
 
-			// Add an HTTP intercepter to Burp.
-			API.http().registerHttpHandler(new ScalpelHttpRequestHandler(API, provider, executor));
+				// Add the response editor to Burp.
+				API
+					.userInterface()
+					.registerHttpResponseEditorProvider(provider);
+
+				// Add an HTTP intercepter to Burp.
+				API
+					.http()
+					.registerHttpHandler(
+						new ScalpelHttpRequestHandler(API, provider, executor)
+					);
+			}
+
+			// Log that the extension has finished loading.
+			TraceLogger.log(logger, "Initialized scalpel successfully.");
+		} catch (Exception e) {
+			TraceLogger.logError(logger, "Failed to initialize scalpel:");
+			TraceLogger.logStackTrace(logger, e);
 		}
-
-		// Log that the extension has finished loading.
-		logger.logToOutput("Initialized successfully.");
 	}
 }
