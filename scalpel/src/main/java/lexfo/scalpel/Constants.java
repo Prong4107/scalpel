576c99d5714a78ca132de4aaef7cdef660f6be12
Noël Maccary <n.maccary@lexfo.fr>
2023-03-27 11:40:41 +0200

    feature(config): Better persistence.

--- scalpel/src/main/java/lexfo/scalpel/Constants.java
+++ scalpel/src/main/java/lexfo/scalpel/Constants.java
@@ -8,30 +8,36 @@ public class Constants {
 	/**
     Callback prefix for request editors.
   */
-	public static final String REQ_EDIT_PREFIX = "req_edit_";
+	public static final String REQ_EDIT_PREFIX = "_req_edit_";
 
 	/**
     Callback prefix for response editors.
   */
-	public static final String RES_EDIT_PREFIX = "res_edit_";
+	public static final String RES_EDIT_PREFIX = "_res_edit_";
 
 	/**
     Callback prefix for HttpMessage-to-bytes convertion.
   */
 	public static final String IN_PREFIX = "in_";
 
+	public static final String IN_SUFFIX = "in";
+
 	/**
-    Callback prefix for bytes-to-HttpMessage convertion.
+    Callback prefix for bytes to HttpMessage convertion.
   */
 	public static final String OUT_PREFIX = "out_";
+
+	public static final String OUT_SUFFIX = "out";
 
 	/**
     Callback prefix for request intercepters.
   */
-	public static final String REQ_CB_NAME = "request";
+	public static final String REQ_CB_NAME = "_request";
 
 	/**
     Callback prefix for response intercepters.
   */
-	public static final String RES_CB_NAME = "response";
+	public static final String RES_CB_NAME = "_response";
+
+	public static final String PERSISTENCE_KEY = "scalpelScript";
 }
