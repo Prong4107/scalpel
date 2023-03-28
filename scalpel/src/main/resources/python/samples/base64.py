09f6b1227a6ed4499567f649654ed796b5227c83
Noël Maccary <n.maccary@lexfo.fr>
2023-03-28 15:02:05 +0200

    refactor(samples): Refactor base64 for framework

--- scalpel/src/main/resources/python/samples/base64.py
+++ scalpel/src/main/resources/python/samples/base64.py
@@ -1,59 +1,43 @@
-import traceback
-from sys import _getframe
-try:
-    from pyscalpel.burp.http_request import IHttpRequest, HttpRequest
-    from pyscalpel.burp.http_response import IHttpResponse, HttpResponse
-    from pyscalpel.burp.byte_array import IByteArray, ByteArray
-    from pyscalpel.burp.logging import Logging
-    from pyscalpel.utils import new_response, byte_array, to_bytes, get_bytes, new_request, logger
-    from base64 import b64decode, b64encode
+from pyscalpel.burp.http_request import IHttpRequest
+from pyscalpel.burp.http_response import IHttpResponse
+from pyscalpel.utils import byte_array, to_bytes, get_bytes, new_request, new_response
+from base64 import b64decode, b64encode
+import binascii
 
 
-    # Test script that adds debug headers
-
-    # TODO: Find why httpRequest.withUpdatedHeader() is broken ?
-
-    def fun_name(  ):
-        return _getframe(1).f_code.co_name
-
-    def req_edit_in_Scalpel(req: IHttpRequest):
-        try:
-            logger.logToOutput(f"Python: {fun_name()}() called")
-            body_bytes = b64decode(get_bytes(req.body()))
-            new_body = byte_array(body_bytes)
-            new_req = req.withBody(new_body)
-            new_bytes = to_bytes(new_req)
-            return new_bytes
-        except Exception as ex:
-            logger.logToOutput(traceback.format_exc())
-            logger.logToError(f"Python: {fun_name()}() error:\n\t{ex}")
+def req_edit_in(req: IHttpRequest) -> bytes:
+    try:
+        body_bytes = b64decode(get_bytes(req.body()), validate=True)
+    except binascii.Error:
         return to_bytes(req)
-        
-    def req_edit_out_Scalpel(req: IHttpRequest, text: bytes):
-        try:
-            logger.logToOutput(f"Python: {fun_name()}() called")
-            req = new_request(bytes(text))
-            body_bytes = b64encode(get_bytes(req.body()))
-            new_body = byte_array(body_bytes)
-            new_req = req.withBody(new_body)
-            new_bytes = to_bytes(new_req)
-            return new_bytes
-        except Exception as ex:
-            logger.logToOutput(traceback.format_exc())
-            logger.logToError(f"Python: {fun_name()}() error:\n\t{ex}")
 
-    def res_edit_in_Scalpel(res: IHttpResponse) -> bytes | None:
-        try:
-            logger.logToOutput(f"Python: {fun_name()}() called")
-            body_bytes = b64decode(get_bytes(res.body()))
-            new_body = byte_array(body_bytes)
-            new_res = res.withBody(new_body)
-            new_bytes = to_bytes(new_res)
-            return new_bytes
-        except Exception as ex:
-            logger.logToOutput(traceback.format_exc())
-            logger.logToError(f"Python: {fun_name()}() error:\n\t{ex}")
-except Exception as global_ex:
-    __logger__.logToOutput("Couldn't load script:")
-    __logger__.logToOutput(global_ex)
-    __logger__.logToOutput(traceback.format_exc())
+    new_body = byte_array(body_bytes)
+    new_req = req.withBody(new_body)
+    new_bytes = to_bytes(new_req)
+    return new_bytes
+
+
+def req_edit_out(req: IHttpRequest, text: bytes) -> bytes:
+    req = new_request(bytes(text))
+    body_bytes = b64encode(get_bytes(req.body()))
+    new_body = byte_array(body_bytes)
+    new_req = req.withBody(new_body)
+    new_bytes = to_bytes(new_req)
+    return new_bytes
+
+
+def res_edit_in(res: IHttpResponse) -> bytes:
+    body_bytes = b64decode(get_bytes(res.body()))
+    new_body = byte_array(body_bytes)
+    new_res = res.withBody(new_body)
+    new_bytes = to_bytes(new_res)
+    return new_bytes
+
+
+def res_edit_out(res: IHttpResponse, text: bytes) -> bytes:
+    res = new_response(bytes(text))
+    body_bytes = b64encode(get_bytes(res.body()))
+    new_body = byte_array(body_bytes)
+    new_res = res.withBody(new_body)
+    new_bytes = to_bytes(new_res)
+    return new_bytes
