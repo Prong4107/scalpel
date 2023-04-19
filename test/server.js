b226448c7999170aa7a41c0a609d5f4a05e62cfd
Noël Maccary <n.maccary@lexfo.fr>
2023-04-19 16:38:01 +0200

    test: Add raw echo route

--- test/server.js
+++ test/server.js
@@ -2,6 +2,8 @@ const { urlencoded } = require('express');
 
 const app = require('express')()
 app.use(require('body-parser').raw({ type: String }))
+
+// Known issue: duplicate headers are not supported
 jsonifyRequest =  (req) => ({
     url: req.url,
     url_decoded: decodeURIComponent(req.url),
@@ -24,12 +26,25 @@ app.all('/base64', (req, res) => {
     res.send(Buffer.from(`Received in base64:\n-----\n${decoded}\n-----`).toString('base64'));
 })
 
-app.all('/', (req, res) => {
+app.all('/json', (req, res) => {
     console.log("Received")
     // Remove date header to ensure identical requests returns the exact same response
     res.setHeader("Date", "[REDACTED]")
     res.send(jsonifyRequest(req))
 })
 
-app.listen(3000)
+app.all('/echo', (req, res) => {
+    console.log("Received")
+    // Remove date header to ensure identical requests returns the exact same response
+    res.setHeader("Date", "[REDACTED]")
+    res.write("HEADERS:\n");
+    rawHeader = req.rawHeaders;
+    for (let i = 0; i < rawHeader.length; i += 2) {
+        res.write(`${rawHeader[i]}: ${rawHeader[i + 1]}\n`);
+    }
+    res.write("\nBODY:\n");
+    res.write(req.body);
+    res.end();
+})
 
+app.listen(3000)
