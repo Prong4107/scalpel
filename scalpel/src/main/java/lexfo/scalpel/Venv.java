e3c7c545854aa2ccdefc0092ad792082e361a85f
Noël Maccary <n.maccary@lexfo.fr>
2023-04-19 10:53:17 +0200

    fix(config): Handle IO errors properly.

--- scalpel/src/main/java/lexfo/scalpel/Venv.java
+++ scalpel/src/main/java/lexfo/scalpel/Venv.java
@@ -4,10 +4,10 @@ package lexfo.scalpel;
  * The Venv class is used to manage Python virtual environments.
  */
 import java.io.IOException;
+import java.nio.file.FileSystemException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.Callable;
 
 /**
  * Manage Python virtual environments.
@@ -20,34 +20,26 @@ public class Venv {
 	 * @param path The path to the virtual environment directory.
 	 * @return The exit code of the "python3 -m venv" command.
 	 */
-	public static int create(String path) {
+	public static int create(String path)
+		throws IOException, InterruptedException {
 		// Create the directory for the virtual environment
 		Path venvPath = Paths.get(path);
-		try {
-			Files.createDirectories(venvPath);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return -1;
-		}
-		try {
-			// Create the virtual environment using the "python3 -m venv" command
-			ProcessBuilder processBuilder = new ProcessBuilder(
-				"python3",
-				"-m",
-				"venv",
-				venvPath.toString()
-			);
-			Process process = processBuilder.start();
+		Files.createDirectories(venvPath);
 
-			// Wait for the virtual environment creation to complete and get the exit code
-			int exitCode = process.waitFor();
+		// Create the virtual environment using the "python3 -m venv" command
+		ProcessBuilder processBuilder = new ProcessBuilder(
+			"python3",
+			"-m",
+			"venv",
+			venvPath.toString()
+		);
+		Process process = processBuilder.start();
 
-			// Return the exit code (0 indicates success)
-			return exitCode;
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-			return -1;
-		}
+		// Wait for the virtual environment creation to complete and get the exit code
+		int exitCode = process.waitFor();
+
+		// Return the exit code (0 indicates success)
+		return exitCode;
 	}
 
 	/**
@@ -56,7 +48,8 @@ public class Venv {
 	 * @param path The path to the virtual environment directory.
 	 * @return The exit code of the "pip install ..." command.
 	 */
-	public static int createAndInstallDefaults(String path) {
+	public static int createAndInstallDefaults(String path)
+		throws IOException, InterruptedException {
 		// Create the virtual environment
 		int exitCode = create(path);
 		if (exitCode != 0) {
@@ -95,7 +88,7 @@ public class Venv {
 	 * @return The exit code of the "pip install ..." command.
 	 */
 	public static Thread install_background(String path, String pkg) {
-		Thread thread = new Thread(() -> install(path, pkg));
+		Thread thread = new Thread(() -> IO.ioWrap(() -> install(path, pkg)));
 		thread.start();
 		return thread;
 	}
@@ -107,28 +100,24 @@ public class Venv {
 	 * @param pkg The name of the package to install.
 	 * @return The exit code of the "pip install ..." command.
 	 */
-	public static int install(String path, String pkg) {
-		try {
-			// Install the package using the "pip install" command
-			ProcessBuilder processBuilder = new ProcessBuilder(
-				"pip",
-				"install",
-				pkg,
-				"-t",
-				getSitePackagesPath(path).toString()
-			);
-			processBuilder.directory(Paths.get(path).toFile());
-			Process process = processBuilder.start();
+	public static int install(String path, String pkg)
+		throws IOException, InterruptedException {
+		// Install the package using the "pip install" command
+		ProcessBuilder processBuilder = new ProcessBuilder(
+			"pip",
+			"install",
+			pkg,
+			"-t",
+			getSitePackagesPath(path).toString()
+		);
+		processBuilder.directory(Paths.get(path).toFile());
+		Process process = processBuilder.start();
 
-			// Wait for the package installation to complete and get the exit code
-			int exitCode = process.waitFor();
+		// Wait for the package installation to complete and get the exit code
+		int exitCode = process.waitFor();
 
-			// Return the exit code (0 indicates success)
-			return exitCode;
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-			return -1;
-		}
+		// Return the exit code (0 indicates success)
+		return exitCode;
 	}
 
 	protected static final class PackageInfo {
@@ -137,18 +126,17 @@ public class Venv {
 		public String version;
 	}
 
-	private static Path getSitePackagesPath(String venvPath) {
+	private static Path getSitePackagesPath(String venvPath)
+		throws IOException {
 		// Find the sites-package directory path as in: <path>/lib/python*/site-packages
-		return IO.ioWrap(() ->
-			Files
-				.walk(Paths.get(venvPath, "lib"))
-				.filter(Files::isDirectory)
-				.filter(p -> p.getFileName().toString().startsWith("python"))
-				.filter(p -> Files.exists(p.resolve("site-packages")))
-				.findFirst()
-				.get()
-				.resolve("site-packages")
-		);
+		return Files
+			.walk(Paths.get(venvPath, "lib"))
+			.filter(Files::isDirectory)
+			.filter(p -> p.getFileName().toString().startsWith("python"))
+			.filter(p -> Files.exists(p.resolve("site-packages")))
+			.findFirst()
+			.get()
+			.resolve("site-packages");
 	}
 
 	/**
@@ -157,7 +145,8 @@ public class Venv {
 	 * @param path The path to the virtual environment directory.
 	 * @return The list of installed packages.
 	 */
-	public static PackageInfo[] getInstalledPackages(String path) {
+	public static PackageInfo[] getInstalledPackages(String path)
+		throws FileSystemException, IOException {
 		Path sitesPackagesPath = getSitePackagesPath(path);
 
 		ProcessBuilder processBuilder = new ProcessBuilder(
@@ -174,19 +163,12 @@ public class Venv {
 		);
 
 		// Launch and parse the JSON output using Jackson
-		try {
-			Process process = processBuilder.start();
+		Process process = processBuilder.start();
 
-			// Read the JSON output
-			String jsonData = new String(
-				process.getInputStream().readAllBytes()
-			);
+		// Read the JSON output
+		String jsonData = new String(process.getInputStream().readAllBytes());
 
-			// Parse the JSON output
-			return IO.readJSON(jsonData, PackageInfo[].class);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		// Parse the JSON output
+		return IO.readJSON(jsonData, PackageInfo[].class);
 	}
 }
