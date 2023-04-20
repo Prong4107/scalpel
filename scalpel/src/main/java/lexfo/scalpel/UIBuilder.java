4e83396046f8bf34f35806d81408e0e14d819cf5
Noël Maccary <n.maccary@lexfo.fr>
2023-04-20 15:18:01 +0200

    refactor(UI): Restore debug interpreter tab.

--- scalpel/src/main/java/lexfo/scalpel/UIBuilder.java
+++ scalpel/src/main/java/lexfo/scalpel/UIBuilder.java
@@ -1,7 +1,10 @@
 package lexfo.scalpel;
 
+import burp.api.montoya.logging.Logging;
 import burp.api.montoya.ui.Theme;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 
 /**
 	Provides methods for constructing the Burp Suite UI.
@@ -21,5 +24,61 @@ public class UIBuilder {
 		Theme theme
 	) {
 		return new ConfigTab(executor, config, theme).uiComponent();
+	}
+
+	/**
+		Constructs the debug Python testing Burp tab.
+		@param executor The ScalpelExecutor object to use.
+		@param logger The Logging object to use.
+		@return The constructed tab.
+	*/
+	public static final Component constructScalpelInterpreterTab(
+		ScalpelExecutor executor,
+		Logging logger
+	) {
+		// Split pane
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		JSplitPane scriptingPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JTextArea outputArea = new JTextArea();
+		JEditorPane editorPane = new JEditorPane();
+		JButton button = new JButton("Run script.");
+
+		button.addActionListener((ActionEvent e) -> {
+			logger.logToOutput("Clicked button");
+			final String scriptContent = editorPane.getText();
+			try {
+				final String[] scriptOutput = executor.evalAndCaptureOutput(
+					scriptContent
+				);
+
+				var txt = String.format(
+					"stdout:\n------------------\n%s\n------------------\n\nstderr:\n------------------\n%s",
+					scriptOutput[0],
+					scriptOutput[1]
+				);
+
+				outputArea.setText(txt);
+			} catch (Exception exception) {
+				outputArea.setText(exception.toString());
+			}
+			logger.logToOutput("Handled action.");
+		});
+
+		editorPane.setText(
+			"""
+print('This goes in stdout')
+print('This goes in stderr', file=sys.stderr)
+"""
+		);
+		outputArea.setEditable(false);
+		scriptingPane.setLeftComponent(new JScrollPane(editorPane));
+		scriptingPane.setRightComponent(new JScrollPane(outputArea));
+		scriptingPane.setResizeWeight(0.5);
+
+		splitPane.setResizeWeight(1);
+		splitPane.setLeftComponent(scriptingPane);
+		splitPane.setRightComponent(button);
+
+		return splitPane;
 	}
 }
