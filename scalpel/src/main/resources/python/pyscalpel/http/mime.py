64fefa9c47f998be6668506adb75aca8daf857cf
Noël Maccary <n.maccary@lexfo.fr>
2023-05-16 10:34:45 +0200

    test(mime): Add unit tests form MIME utils.

--- scalpel/src/main/resources/python/pyscalpel/http/mime.py
+++ scalpel/src/main/resources/python/pyscalpel/http/mime.py
@@ -1,5 +1,4 @@
 import re
-import unittest
 from urllib.parse import quote as urllibquote
 from requests_toolbelt.multipart.encoder import MultipartEncoder, encode_with
 from typing import Sequence
@@ -103,152 +102,150 @@ def get_header_value_without_params(header_value: str) -> str:
     return header_value.split(";", maxsplit=1)[0].strip()
 
 
-class ParseMimeHeaderTestCase(unittest.TestCase):
-    def test_empty_string(self):
-        header_str = ""
-        result = parse_mime_header_value(header_str)
-        self.assertEqual(result, [])
-
-    def test_single_pair_no_quotes(self):
-        header_str = "key=value"
-        result = parse_mime_header_value(header_str)
-        expected = [("key", "value")]
-        self.assertEqual(result, expected)
-
-    def test_single_pair_with_quotes(self):
-        header_str = 'key="value"'
-        result = parse_mime_header_value(header_str)
-        expected = [("key", "value")]
-        self.assertEqual(result, expected)
-
-    def test_multiple_pairs(self):
-        header_str = "key1=value1; key2=value2; key3=value3"
-        result = parse_mime_header_value(header_str)
-        expected = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
-        self.assertEqual(result, expected)
-
-    def test_mixed_quotes(self):
-        header_str = 'key1="value1"; key2=value2; key3="value3"'
-        result = parse_mime_header_value(header_str)
-        expected = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
-        self.assertEqual(result, expected)
-
-    def test_value_with_semicolon_and_quotes(self):
-        header_str = 'key="value;with;semicolons"; key2=value2'
-        result = parse_mime_header_value(header_str)
-        expected = [("key", "value;with;semicolons"), ("key2", "value2")]
-        self.assertEqual(result, expected)
-
-
-class HeaderParsingTestCase(unittest.TestCase):
-    def test_unparse_header_value(self):
-        parsed_header = [
-            ("Content-Type", "text/html"),
-            ("charset", "utf-8"),
-            ("boundary", "abcdef"),
-        ]
-        expected = 'text/html; charset="utf-8"; boundary="abcdef"'
-        result = unparse_header_value(parsed_header)
-        self.assertEqual(result, expected)
-
-    def test_unparse_header_value_single_parameter(self):
-        parsed_header = [
-            ("Content-Type", "text/html"),
-            ("charset", "utf-8"),
-        ]
-        expected = 'text/html; charset="utf-8"'
-        result = unparse_header_value(parsed_header)
-        self.assertEqual(result, expected)
-
-    def test_parse_header(self):
-        key = "Content-Type"
-        value = 'text/html; charset="utf-8"; boundary="abcdef"'
-        expected = [
-            ("Content-Type", 'text/html; charset="utf-8"; boundary="abcdef"'),
-            ("charset", "utf-8"),
-            ("boundary", "abcdef"),
-        ]
-        result = parse_header(key, value)
-        self.assertEqual(result, expected)
-
-    def test_parse_header_single_parameter(self):
-        key = "Content-Type"
-        value = 'text/html; charset="utf-8"'
-        expected = [
-            ("Content-Type", 'text/html; charset="utf-8"'),
-            ("charset", "utf-8"),
-        ]
-        result = parse_header(key, value)
-        self.assertEqual(result, expected)
-
-
-class BoundaryExtractionTestCase(unittest.TestCase):
-    def test_extract_boundary(self):
-        content_type = 'multipart/form-data; boundary="abcdefg12345"'
-        encoding = "utf-8"
-        expected = b"abcdefg12345"
-        result = extract_boundary(content_type, encoding)
-        self.assertEqual(result, expected)
-
-    def test_extract_boundary_no_quotes(self):
-        content_type = "multipart/form-data; boundary=abcdefg12345"
-        encoding = "utf-8"
-        expected = b"abcdefg12345"
-        result = extract_boundary(content_type, encoding)
-        self.assertEqual(result, expected)
-
-    def test_extract_boundary_multiple_parameters(self):
-        content_type = 'multipart/form-data; charset=utf-8; boundary="abcdefg12345"'
-        encoding = "utf-8"
-        expected = b"abcdefg12345"
-        result = extract_boundary(content_type, encoding)
-        self.assertEqual(result, expected)
-
-    def test_extract_boundary_missing_boundary(self):
-        content_type = "multipart/form-data; charset=utf-8"
-        encoding = "utf-8"
-        with self.assertRaises(RuntimeError):
-            extract_boundary(content_type, encoding)
-
-    def test_extract_boundary_unexpected_mimetype(self):
-        content_type = 'application/json; boundary="abcdefg12345"'
-        encoding = "utf-8"
-        with self.assertRaises(RuntimeError):
-            extract_boundary(content_type, encoding)
-
-
-class TestHeaderParams(unittest.TestCase):
-    def setUp(self):
-        self.params: Sequence[tuple[str, str | None]] = [
-            ("Content-Disposition", "form-data"),
-            ("name", "file"),
-            ("filename", "index.html"),
-        ]
-
-    def test_find_header_param(self):
-        # Testing existing key
-        self.assertEqual(find_header_param(self.params, "name"), ("name", "file"))
-
-        self.assertEqual(
-            find_header_param(self.params, "filename"), ("filename", "index.html")
-        )
-
-        # Testing non-existing key
-        self.assertEqual(find_header_param(self.params, "non-existing-key"), None)
-
-    def test_update_header_param(self):
-        # Testing update of existing key
-        updated_params = update_header_param(self.params, "name", "updated_file")
-        self.assertIn(("name", "updated_file"), updated_params)
-
-        # Testing addition of new key
-        updated_params = update_header_param(self.params, "new-key", "new-value")
-        self.assertIn(("new-key", "new-value"), updated_params)
-
-        # Testing update with None value
-        updated_params = update_header_param(self.params, "name", None)
-        self.assertIn(("name", None), updated_params)
-
-
 if __name__ == "__main__":
+    import unittest
+
+    class ParseMimeHeaderTestCase(unittest.TestCase):
+        def test_empty_string(self):
+            header_str = ""
+            result = parse_mime_header_value(header_str)
+            self.assertEqual(result, [])
+
+        def test_single_pair_no_quotes(self):
+            header_str = "key=value"
+            result = parse_mime_header_value(header_str)
+            expected = [("key", "value")]
+            self.assertEqual(result, expected)
+
+        def test_single_pair_with_quotes(self):
+            header_str = 'key="value"'
+            result = parse_mime_header_value(header_str)
+            expected = [("key", "value")]
+            self.assertEqual(result, expected)
+
+        def test_multiple_pairs(self):
+            header_str = "key1=value1; key2=value2; key3=value3"
+            result = parse_mime_header_value(header_str)
+            expected = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
+            self.assertEqual(result, expected)
+
+        def test_mixed_quotes(self):
+            header_str = 'key1="value1"; key2=value2; key3="value3"'
+            result = parse_mime_header_value(header_str)
+            expected = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
+            self.assertEqual(result, expected)
+
+        def test_value_with_semicolon_and_quotes(self):
+            header_str = 'key="value;with;semicolons"; key2=value2'
+            result = parse_mime_header_value(header_str)
+            expected = [("key", "value;with;semicolons"), ("key2", "value2")]
+            self.assertEqual(result, expected)
+
+    class HeaderParsingTestCase(unittest.TestCase):
+        def test_unparse_header_value(self):
+            parsed_header = [
+                ("Content-Type", "text/html"),
+                ("charset", "utf-8"),
+                ("boundary", "abcdef"),
+            ]
+            expected = 'text/html; charset="utf-8"; boundary="abcdef"'
+            result = unparse_header_value(parsed_header)
+            self.assertEqual(result, expected)
+
+        def test_unparse_header_value_single_parameter(self):
+            parsed_header = [
+                ("Content-Type", "text/html"),
+                ("charset", "utf-8"),
+            ]
+            expected = 'text/html; charset="utf-8"'
+            result = unparse_header_value(parsed_header)
+            self.assertEqual(result, expected)
+
+        def test_parse_header(self):
+            key = "Content-Type"
+            value = 'text/html; charset="utf-8"; boundary="abcdef"'
+            expected = [
+                ("Content-Type", 'text/html; charset="utf-8"; boundary="abcdef"'),
+                ("charset", "utf-8"),
+                ("boundary", "abcdef"),
+            ]
+            result = parse_header(key, value)
+            self.assertEqual(result, expected)
+
+        def test_parse_header_single_parameter(self):
+            key = "Content-Type"
+            value = 'text/html; charset="utf-8"'
+            expected = [
+                ("Content-Type", 'text/html; charset="utf-8"'),
+                ("charset", "utf-8"),
+            ]
+            result = parse_header(key, value)
+            self.assertEqual(result, expected)
+
+    class BoundaryExtractionTestCase(unittest.TestCase):
+        def test_extract_boundary(self):
+            content_type = 'multipart/form-data; boundary="abcdefg12345"'
+            encoding = "utf-8"
+            expected = b"abcdefg12345"
+            result = extract_boundary(content_type, encoding)
+            self.assertEqual(result, expected)
+
+        def test_extract_boundary_no_quotes(self):
+            content_type = "multipart/form-data; boundary=abcdefg12345"
+            encoding = "utf-8"
+            expected = b"abcdefg12345"
+            result = extract_boundary(content_type, encoding)
+            self.assertEqual(result, expected)
+
+        def test_extract_boundary_multiple_parameters(self):
+            content_type = 'multipart/form-data; charset=utf-8; boundary="abcdefg12345"'
+            encoding = "utf-8"
+            expected = b"abcdefg12345"
+            result = extract_boundary(content_type, encoding)
+            self.assertEqual(result, expected)
+
+        def test_extract_boundary_missing_boundary(self):
+            content_type = "multipart/form-data; charset=utf-8"
+            encoding = "utf-8"
+            with self.assertRaises(RuntimeError):
+                extract_boundary(content_type, encoding)
+
+        def test_extract_boundary_unexpected_mimetype(self):
+            content_type = 'application/json; boundary="abcdefg12345"'
+            encoding = "utf-8"
+            with self.assertRaises(RuntimeError):
+                extract_boundary(content_type, encoding)
+
+    class TestHeaderParams(unittest.TestCase):
+        def setUp(self):
+            self.params: Sequence[tuple[str, str | None]] = [
+                ("Content-Disposition", "form-data"),
+                ("name", "file"),
+                ("filename", "index.html"),
+            ]
+
+        def test_find_header_param(self):
+            # Testing existing key
+            self.assertEqual(find_header_param(self.params, "name"), ("name", "file"))
+
+            self.assertEqual(
+                find_header_param(self.params, "filename"), ("filename", "index.html")
+            )
+
+            # Testing non-existing key
+            self.assertEqual(find_header_param(self.params, "non-existing-key"), None)
+
+        def test_update_header_param(self):
+            # Testing update of existing key
+            updated_params = update_header_param(self.params, "name", "updated_file")
+            self.assertIn(("name", "updated_file"), updated_params)
+
+            # Testing addition of new key
+            updated_params = update_header_param(self.params, "new-key", "new-value")
+            self.assertIn(("new-key", "new-value"), updated_params)
+
+            # Testing update with None value
+            updated_params = update_header_param(self.params, "name", None)
+            self.assertIn(("name", None), updated_params)
+
     unittest.main()
