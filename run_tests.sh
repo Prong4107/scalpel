c705c70401c306df1eb5e943309b7dffcffc70c5
Noël Maccary <n.maccary@lexfo.fr>
2023-05-16 10:35:29 +0200

    fix(tests): NeutralizeJjava imports when testing.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,11 @@
 SRC="
 scalpel/src/main/resources/python/pyscalpel/http/mime.py
+scalpel/src/main/resources/python/pyscalpel/http/body.py
+scalpel/src/main/resources/python/qs/qs.py
 "
 
-python3 $SRC
+export _DO_NOT_IMPORT_JAVA="yes"
+export PATH="scalpel/src/main/resources/python:$PATH"
+for file in $SRC;do
+    python3 "$file"
+done
