6bb37ffec243f5c83c335daefaec97968fbe5912
Noël Maccary <n.maccary@lexfo.fr>
2023-06-02 17:27:51 +0200

    style(encoding): Renamed var to conform to PEP-8

--- scalpel/src/main/resources/python/pyscalpel/encoding.py
+++ scalpel/src/main/resources/python/pyscalpel/encoding.py
@@ -1,24 +1,24 @@
-from mitmproxy.utils import strutils
 from urllib.parse import unquote_to_bytes as urllibdecode
+from mitmproxy.utils import strutils
 
 
 # str/bytes conversion helpers from mitmproxy/http.py:
 # https://github.com/mitmproxy/mitmproxy/blob/main/mitmproxy/http.py#:~:text=def-,_native,-(x%3A
-def always_bytes(x: str | bytes | int) -> bytes:
-    if isinstance(x, int):
-        x = str(x)
-    return strutils.always_bytes(x, "utf-8", "surrogateescape")
+def always_bytes(data: str | bytes | int) -> bytes:
+    if isinstance(data, int):
+        data = str(data)
+    return strutils.always_bytes(data, "utf-8", "surrogateescape")
 
 
-def always_str(x: str | bytes | int) -> str:
-    if isinstance(x, int):
-        return str(x)
-    return strutils.always_str(x, "utf-8", "surrogateescape")
+def always_str(data: str | bytes | int) -> str:
+    if isinstance(data, int):
+        return str(data)
+    return strutils.always_str(data, "utf-8", "surrogateescape")
 
 
-def native(x: bytes) -> str:
+def native(data: bytes) -> str:
     # While headers _should_ be ASCII, it's not uncommon for certain headers to be utf-8 encoded.
-    return x.decode("utf-8", "surrogateescape")
+    return data.decode("utf-8", "surrogateescape")
 
 
 def urlencode_all(data: bytes | str) -> bytes:
