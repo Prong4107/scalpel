1ab4f216147fece6d529362e2b4ed5e09dc1803c
Noël Maccary <n.maccary@lexfo.fr>
2023-06-07 18:05:10 +0200

    feature(Flow): Expose flow

--- scalpel/src/main/resources/python/pyscalpel/http/__init__.py
+++ scalpel/src/main/resources/python/pyscalpel/http/__init__.py
@@ -1,6 +1,7 @@
 from .request import Request, Headers
 from .response import Response
+from .flow import Flow
 
 # from .body import Serializer, JSONFormSerializer, URLEncodedFormSerializer
 
-__all__ = ["Request", "Response", "Headers"]
+__all__ = ["Request", "Response", "Headers", "Flow"]
