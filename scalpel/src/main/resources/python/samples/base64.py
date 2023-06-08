3c239ddf7c2070fad94e93afe5d15555bea40a8c
Noël Maccary <n.maccary@lexfo.fr>
2023-06-08 18:05:37 +0200

    fix(samples): Updated base64 sample

--- scalpel/src/main/resources/python/samples/base64.py
+++ scalpel/src/main/resources/python/samples/base64.py
@@ -7,31 +7,31 @@ from typing import cast
 def req_edit_in(req: Request) -> bytes:
     if req.content:
         try:
-            req.content = b64decode(cast(bytes, req.get_content()), validate=True)
+            req.content = b64decode(req.content, validate=True)
         except binascii.Error:
             pass
 
     return bytes(req)
 
 
-def req_edit_out(_: Request, text: bytes) -> bytes:
+def req_edit_out(_: Request, text: bytes) -> Request:
     req = Request.from_raw(text)
     if req.content:
-        req.content = b64encode(cast(bytes, req.get_content()))
-    return bytes(req)
+        req.content = b64encode(req.content)
+    return req
 
 
 def res_edit_in(res: Response) -> bytes:
     if res.content:
         try:
-            res.content = b64decode(cast(bytes, res.get_content()), validate=True)
+            res.content = b64decode(res.content, validate=True)
         except binascii.Error:
             pass
     return bytes(res)
 
 
-def res_edit_out(_: Response, text: bytes) -> bytes:
+def res_edit_out(_: Response, text: bytes) -> Response:
     res = Response.from_raw(text)
     if res.content:
-        res.content = b64encode(cast(bytes, res.get_content()))
-    return bytes(res)
+        res.content = b64encode(res.content)
+    return res
