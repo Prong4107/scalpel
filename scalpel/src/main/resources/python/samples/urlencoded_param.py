8d674b53f2a953688c46a4c17beeaee14fa87bac
Noël Maccary <n.maccary@lexfo.fr>
2023-06-13 19:14:41 +0200

    fix(samples): Updated urlencoded sample.

--- scalpel/src/main/resources/python/samples/urlencoded_param.py
+++ scalpel/src/main/resources/python/samples/urlencoded_param.py
@@ -1,16 +1,17 @@
 from pyscalpel.http import Request
-from pyscalpel.utils import get_param, urldecode, urlencode_all, update_param
+from pyscalpel.utils import urldecode, urlencode_all
 
 # POC script to edit a fully URL encoded parameter
 
 PARAM_NAME = "filename"
 
 
-def req_edit_in(req: Request) -> bytes | None:
-    param = get_param(req, PARAM_NAME)
+def req_edit_in_filename(req: Request) -> bytes | None:
+    param = req.query.get(PARAM_NAME)
     if param is not None:
-        return urldecode(str.encode(param.value()))
+        return urldecode(param)
 
 
-def req_edit_out(req: Request, text: bytes) -> bytes | None:
-    return bytes(update_param(req, PARAM_NAME, urlencode_all(text)))
+def req_edit_out_filename(req: Request, text: bytes) -> Request | None:
+    req.query[PARAM_NAME] = urlencode_all(text)
+    return req
