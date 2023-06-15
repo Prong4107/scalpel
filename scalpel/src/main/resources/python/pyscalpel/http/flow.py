2bf5cc2cc83b92eadc6069045f086fd52461cd74
Noël Maccary <n.maccary@lexfo.fr>
2023-06-15 10:49:43 +0200

    feature(wrappers): Add path_is match method

--- scalpel/src/main/resources/python/pyscalpel/http/flow.py
+++ scalpel/src/main/resources/python/pyscalpel/http/flow.py
@@ -15,5 +15,12 @@ class Flow:
     response: Response | None = None
     text: bytes | None = None
 
-    def host_is(self, *patterns: str):
+    def host_is(self, *patterns: str) -> bool:
         return host_is(self.host, *patterns)
+
+    def path_is(self, *patterns: str) -> bool:
+        req = self.request
+        if req is None:
+            return False
+
+        return req.path_is(*patterns)
