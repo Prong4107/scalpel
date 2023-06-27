c78d79054902f4b523fc7705f2cf720811a2cdfc
Noël Maccary <n.maccary@lexfo.fr>
2023-06-27 11:59:19 +0200

    build(setup): Set error option and fix typo

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-set -x
+set -e
 
 echo Creating .venv ...
 python3 -m venv .venv
 source ./.venv/bin/activate
 
 if [ -z "$JAVA_HOME" ];then 
-    Setting default JAVA_HOME="$JAVA_HOME" ...
     export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+    echo Setting default JAVA_HOME="$JAVA_HOME" ...
 fi
 
 echo Installing jep ...
