4ac4bfb093c63268a8d770a4a4c547cf9e5d4368
Noël Maccary <nmaccary@lexfo.fr>
2023-07-27 14:59:51 +0200

    refactor(utils): Removed now useless functions

--- scalpel/src/main/resources/python/pyscalpel/utils.py
+++ scalpel/src/main/resources/python/pyscalpel/utils.py
@@ -1,30 +1,8 @@
-from typing import TypeVar
 import inspect
 from pyscalpel.burp_utils import (
-    IHttpParameter,
-    IHttpRequest,
-    get_param as burp_get_param,
-    new_param,
     urldecode,
     urlencode_all,
 )
-from pyscalpel.http import Request
-from pyscalpel.encoding import always_str
-
-
-def get_param(msg: Request, name: str) -> IHttpParameter | None:
-    return burp_get_param(msg.to_burp(), name)
-
-
-GenericRequest = TypeVar("GenericRequest", IHttpRequest, Request)
-
-
-def update_param(
-    req: GenericRequest, name: str | bytes, value: str | bytes
-) -> GenericRequest:
-    if isinstance(req, Request):
-        return Request.from_burp(update_param(req.to_burp(), name, value))
-    return req.withUpdatedParameters([new_param(always_str(name), always_str(value))])
 
 
 def current_function_name() -> str:
@@ -65,8 +43,6 @@ def get_tab_name() -> str:
 
 
 __all__ = [
-    "get_param",
-    "update_param",
     "urldecode",
     "urlencode_all",
     "current_function_name",
