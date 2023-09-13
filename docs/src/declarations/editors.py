04f99b3d72cc833a1dab6fc7fefe60d7fa837f34
Noël Maccary <nmaccary@lexfo.fr>
2023-09-13 14:51:59 +0200

    docs: Fix typo

--- docs/src/declarations/editors.py
+++ docs/src/declarations/editors.py
@@ -1,6 +1,6 @@
 """
     Scalpel allows choosing between normal and binary editors, to do so,
-    the user can apply the `editor` decorator to the `req_edit_in` / `res_edit_int` hook:
+    the user can apply the `editor` decorator to the `req_edit_in` / `res_edit_in` hook:
 """
 from pyscalpel.edit import editor
 
