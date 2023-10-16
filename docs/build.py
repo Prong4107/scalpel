08efc656f9a6b08266b99b51f3af5b0a2e38e2cd
Noël Maccary <nmaccary@lexfo.fr>
2023-10-16 13:57:20 +0200

    CI: Update build script to disabe javadoc

--- docs/build.py
+++ docs/build.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+import argparse
 import shutil
 import subprocess
 from pathlib import Path
 import sys
 import os
 import pdoc
+
+
+parser = argparse.ArgumentParser(description="Build documentation.")
+parser.add_argument(
+    "--no-javadoc", action="store_true", help="Disable Javadoc generation"
+)
+
+args = parser.parse_args()
 
 here = Path(__file__).parent
 pyscalpel_path = here.parent / "scalpel" / "src" / "main" / "resources" / "python"
@@ -28,11 +37,12 @@ if (here / "public").exists():
 
 
 subprocess.run(["hugo"], cwd=here / "src", check=True)
-subprocess.run(["./gradlew", "javadoc"], cwd=here.parent, check=True)
-shutil.copytree(
-    here.parent / "scalpel" / "build" / "docs" / "javadoc",
-    here / "public" / "javadoc",
-    dirs_exist_ok=True,
-)
+if not args.no_javadoc:
+    subprocess.run(["./gradlew", "javadoc"], cwd=here.parent, check=True)
+    shutil.copytree(
+        here.parent / "scalpel" / "build" / "docs" / "javadoc",
+        here / "public" / "javadoc",
+        dirs_exist_ok=True,
+    )
 
 pdoc.pdoc(pyscalpel_path, output_directory=here / "public" / "pdoc")
